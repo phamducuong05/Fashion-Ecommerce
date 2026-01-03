d183d1883a04a064f88e4576bf3587f11a204ce1
phamducuong05 <cuongmerlin@gmail.com>
2026-01-03 09:41:51 +0700

    Erase redundant file

--- server/src/routes/categoryRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCategories } from "../controllers/categoryController";
-
-const router = Router();
-
-// GET /api/categories
-router.get("/", getCategories);
-
-export default router;
